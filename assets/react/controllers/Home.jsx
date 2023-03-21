443cd9f49c31ebbf22e0ef4300c7374b0c29013f
Matthieu Mota <matthieumota@gmail.com>
2023-03-21 18:44:49 +0100

    Mini filter with React

--- assets/react/controllers/Home.jsx
+++ assets/react/controllers/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home({ companies }) {
+    let [search, setSearch] = useState('');
+
     return (
         <div>
             <h2 className="text-blue-500 font-bold text-2xl">Hello React</h2>
 
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
             <ul>
-                {companies.map(company => <li>{company}</li>)}
+                {/* On filtre les companies dont le nom inclut ce qui a été mis dans la recherche (en minuscule) */}
+                {companies.filter(company => company.toLowerCase().includes(search.toLowerCase())).map(company => <li>{company}</li>)}
             </ul>
         </div>
     )
